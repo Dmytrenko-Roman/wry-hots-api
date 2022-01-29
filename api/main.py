28b4651c3d7fff4bf97b2b1aafa1146a88a5cd91
Dmytrenko-Roman <savitarreus@gmail.com>
2022-01-29 19:53:40 +0200

    ❄️ This functions must be implemented in future ❄️

--- api/main.py
+++ api/main.py
@@ -3,6 +3,26 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def index():
+@app.get('/heroes')
+def get_heroes() -> dict:
     return {'data': {'name': 'Arthas'}}
+
+
+@app.get('/heroes/{name}')
+def get_hero_by_name(name: str) -> dict:
+    return {'data': name}
+
+
+@app.post('/heroes/addhero')
+def add_hero() -> dict:
+    return {'data': 'Successfully create a hero'}
+
+
+@app.put('/heroes/{name}')
+def update_hero(name: str) -> dict:
+    return {'data': f'hero with name {name} was updated'}
+
+
+@app.delete('/heroes/{name}')
+def delete_hero(name: str) -> dict:
+    return {'data': f'hero with name {name} was deleted'}
