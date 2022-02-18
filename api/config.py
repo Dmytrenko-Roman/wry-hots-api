a8093ac028a6c70fe84f752276352603ec17b541
Dmytrenko-Roman <savitarreus@gmail.com>
2022-02-18 12:12:41 +0200

    ❄️ Replaced postgre with sqlite ❄️

--- api/config.py
+++ api/config.py
@@ -2,10 +2,10 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_hostname: str
-    database_username: str
-    database_password: str
-    database_name: str
+    # database_hostname: str
+    # database_username: str
+    # database_password: str
+    # database_name: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
